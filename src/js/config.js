85db40991e50d95fb8d80c113b3e033fb5f0a6b2
GoranK89 <goran.kolar7@gmail.com>
2023-06-29 21:16:22 +0200

    Initial comit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const API_KEY = '52dacfd1-35e7-49d5-9c05-5be94ebd73cf';
+export const MODAL_CLOSE_SEC = 2.5;
